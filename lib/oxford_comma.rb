bb69cdfd235be78de226f61b0a7f08143605e5e9
Daniel <dkilcullen25@gmail.com>
2022-02-07 16:16:24 -0600

    commit tree 2f3dd32652e0fc1ee09f29aca0658874550f21d4

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
-
+require 'pry'
+def oxford_comma array
+    if array.length == 1
+        return array[0].to_s
+    elsif array.length == 2
+        new_array = array.each_with_index.map do |item, index|
+            index == array.length - 1 ? "and #{item}" : "#{item} "
+        end
+    else
+        new_array = array.each_with_index.map do |item, index|
+            index == array.length - 1 ? "and #{item}" : "#{item}, "
+        end
+    end
+    new_array.join
 end
+
+# binding.pry
